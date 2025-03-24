5f0b562267722fda1bb94cb7ac6a93830ffeeebb
Sanju <sanjana.b0831@gmail.com>
2025-03-24 11:39:29 +0530

    tuple_functions_added

--- tuple.py
+++ tuple.py
@@ -3,3 +3,16 @@ t=(1,2,3,4,5)
 print(t)
 
 
+# 1.      Write a Python program to unpack a tuple into several variables.
+t=(1,2,3,4,5)
+a,b,c,d,e=t
+print(a,b,c,d,e)
+
+# 2.       Write a  Python program to convert a tuple to a string.
+t=('a','b','c','d','e')
+s=''.join(t)
+print(s)
+
+# 3. Write a Python program to check whether an element exists within a tuple.
+t=(1,2,3,4,5)
+print(3 in t)

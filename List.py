ea45c06a1a7929c4f326d5d8eb2e133ffb6f1339
Sanju <sanjana.b0831@gmail.com>
2025-03-24 11:34:43 +0530

    List_functions_added

--- List.py
+++ List.py
@@ -13,3 +13,20 @@ for i in l1:
         l2.append(i)
 print(l2)
 
+l2=[1,2,3,4,4,2,4,1,7]
+l1=[]
+for i in l2:
+    if i not in l1:
+        l1.append(i)
+print(l1)
+
+l1=[1,2,3,4,5]
+l2=l1.copy()
+print(l2)
+
+l1=['abc', 'xyz', 'aba', '1221']
+count=0
+for i in l1:
+    if len(i)>=2 and i[0]==i[-1]:
+        count+=1
+print(count)

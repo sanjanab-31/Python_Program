3913771ba6c2a79889f7f0387da2b0d6ee4ba850
Sanju <sanjana.b0831@gmail.com>
2025-04-01 09:34:52 +0530

    strings

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,9 @@
-t.append(6)
-# print(t)
+a=input("Enter a string: ")
+# if len(a)%2==0:
+#     a1=a[:len(a)//2]
+#     a2=a[len(a)//2:]
+#     print(a1,a2)
+# else:   
+#     a1=a[:len(a)//2+1]
+#     a2=a[len(a)//2+1:]
+#     print(a1,a2)

d74fffeed2f92001c26c735003ac2ca0d6ec6e88
Sanju <sanjana.b0831@gmail.com>
2025-04-01 09:48:17 +0530

    tuple programs added

--- tuple.py
+++ tuple.py
@@ -16,3 +16,22 @@ print(s)
 # 3. Write a Python program to check whether an element exists within a tuple.
 t=(1,2,3,4,5)
 print(3 in t)
+
+
+# Tuple with the same product
+# l1=[1,2,3,4,5]
+# l2=[6,7,8,9,10]
+# l3=[]
+# for i in range(len(l1)):
+#     l3.append(l1[i]*l2[i])
+# print(l3)
+
+# Copy speccific elements from one tuple to a new tuple
+# l1=(1,2,3,4,5,6,7,8,9,10)
+# l2=()
+# for i in range(len(l1)):
+#     if i%2==0:
+#         l2+=l1[i],
+# print(l2)
+
+
